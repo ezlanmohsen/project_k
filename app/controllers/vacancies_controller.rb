2070b63c81efe26edb10bfa6d8b3ad370a7e1b95
ezlan <ezlanmohsen@gmail.com>
2018-10-13 19:03:45 +0800

    Search filters using tag. Right now only 1-dimensional for demo purpose

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -18,7 +18,25 @@ class VacanciesController < ApplicationController
 	def show
 		@vacancy = Vacancy.find(params[:id])
 		@company = Company.find(@vacancy.company.id)
-		@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry)
+		@degrees = Industry.all
+		@knowledges = Topic.all
+		@skills = Skill.all
+		@activities = Activity.all
+
+	  	if params[:degree] 
+	      	@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry).where(degree_id: params[:degree])
+	    elsif params[:knowledge]
+	    	@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry).where(top_knowledge_id: params[:knowledge])
+	    elsif params[:activity]
+	    	@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry).where(top_knowledge_id: params[:activity])
+	    elsif params[:skill]
+    		@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry).where(top_knowledge_id: params[:skill])
+    	else
+			@jobseekers = Jobseeker.where(location_id: @company.state).where("we1_id LIKE (?) OR we2_id LIKE (?) or we3_id LIKE (?)", @company.industry, @company.industry, @company.industry)
+	    end
+
+
+
 
 
 	end
